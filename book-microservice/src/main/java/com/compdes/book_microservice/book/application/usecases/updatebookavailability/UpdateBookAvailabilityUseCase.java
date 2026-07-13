92e4894b631b91274da209510790810d69182490
Isaac03483 <levihernandez2501@gmail.com>
2026-07-13 10:19:04 -0600

    changed findUseCase to inputPort

--- book-microservice/src/main/java/com/compdes/book_microservice/book/application/usecases/updatebookavailability/UpdateBookAvailabilityUseCase.java
+++ book-microservice/src/main/java/com/compdes/book_microservice/book/application/usecases/updatebookavailability/UpdateBookAvailabilityUseCase.java
@@ -1,8 +1,8 @@
 package com.compdes.book_microservice.book.application.usecases.updatebookavailability;
 
+import com.compdes.book_microservice.book.application.inputports.FindingBookByIdInputPort;
 import com.compdes.book_microservice.book.application.inputports.UpdatingBookAvailabilityInputPort;
 import com.compdes.book_microservice.book.application.outputports.persistence.StoringBookOutputPort;
-import com.compdes.book_microservice.book.application.usecases.findbook.FindBookByIdUseCase;
 import com.compdes.book_microservice.book.domain.Book;
 import com.compdes.book_microservice.common.application.annotations.UseCase;
 import com.compdes.book_microservice.common.application.exceptions.EntityNotFoundException;
@@ -12,17 +12,17 @@ import java.util.UUID;
 @UseCase
 public class UpdateBookAvailabilityUseCase implements UpdatingBookAvailabilityInputPort {
 
-    private final FindBookByIdUseCase findBookByIdUseCase;
+    private final FindingBookByIdInputPort findingBookByIdInputPort;
     private final StoringBookOutputPort storingBookOutputPort;
 
-    public UpdateBookAvailabilityUseCase(FindBookByIdUseCase findBookByIdUseCase, StoringBookOutputPort storingBookOutputPort) {
-        this.findBookByIdUseCase = findBookByIdUseCase;
+    public UpdateBookAvailabilityUseCase(FindingBookByIdInputPort findingBookByIdInputPort, StoringBookOutputPort storingBookOutputPort) {
+        this.findingBookByIdInputPort = findingBookByIdInputPort;
         this.storingBookOutputPort = storingBookOutputPort;
     }
 
     @Override
     public Book updateBookAvailability(UUID id) throws EntityNotFoundException {
-        Book book = this.findBookByIdUseCase.findBookById(id);
+        Book book = this.findingBookByIdInputPort.findBookById(id);
 
         // TODO: PREGUNTAR
         Book bookUpdated = new Book(
